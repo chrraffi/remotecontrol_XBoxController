4ccb6c9c98b58fbe10df6e237aedbaa8f49140ad
chrraffi <30281280+chrraffi@users.noreply.github.com>
2022-04-30 15:53:49 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,23 +17,30 @@ bluetooth.onBluetoothDisconnected(function () {
         `)
 })
 function MoveController () {
-    if (YCoord == 0) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 40)
-    } else if (YCoord == 4) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 40)
-    } else if (XCoord == 0) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Left, 20)
-    } else if (XCoord == 4) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
-    } else {
-        Kitronik_Move_Motor.stop()
-    }
     if (Button_Value == "A") {
         Kitronik_Move_Motor.stop()
         Button_Value = "."
     } else if (Button_Value == "B") {
         Kitronik_Move_Motor.beepHorn()
+        Claw_Angle += 10
+        Kitronik_Move_Motor.writeServoPin(Kitronik_Move_Motor.ServoSelection.servo1, Claw_Angle)
         Button_Value = "."
+    } else if (Button_Value == "X") {
+        Claw_Angle += -10
+        Kitronik_Move_Motor.writeServoPin(Kitronik_Move_Motor.ServoSelection.servo1, Claw_Angle)
+        Button_Value = "."
+    } else {
+        if (YCoord == 0) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 40)
+        } else if (YCoord == 4) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 40)
+        } else if (XCoord == 0) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Left, 20)
+        } else if (XCoord == 4) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Right, 20)
+        } else {
+            Kitronik_Move_Motor.stop()
+        }
     }
 }
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
@@ -51,12 +58,16 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.SemiColon), function (
 let value = ""
 let key = ""
 let Button_Value = ""
+let Claw_Angle = 0
 let YCoord = 0
 let XCoord = 0
 bluetooth.startUartService()
 bluetooth.setTransmitPower(7)
 XCoord = 2
 YCoord = 2
+Claw_Angle = 40
+Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Wide)
+Kitronik_Move_Motor.writeServoPin(Kitronik_Move_Motor.ServoSelection.servo1, Claw_Angle)
 basic.forever(function () {
 	
 })
